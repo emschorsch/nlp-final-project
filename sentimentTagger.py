ffca95bd662e5388e413b75282261be90c47c916
tino <thejustincosentino@gmail.com>
2012-11-27 23:00:05 -0500

    added some eval to sentimentTagger

--- sentimentTagger.py
+++ sentimentTagger.py
@@ -6,7 +6,8 @@ tweet.
 1) A simple count of the number of sentiment types
 2) something complicated
 """
-
+from collections import defaultdict
+from sentimentWords import getSentimentWords
 
 #some global values
 TRAINFILE = 'outputTestA.txt'
@@ -24,12 +25,36 @@ def extractData( fileName ):
   return trainData
 
 
+def useSentimentWordsOnly(sentimentWords,trainData):
+  taggedData = []
+  for instance in trainData:
+    tweet = instance[TWEET]
+    strongNeg = weakNeg = strongPos = weakPos = 0
+    for word.strip('!@#$%^&*(),.?\"') in tweet:
+      
+      # counts the number of pos and neg words in the tweet
+      if sentimentWords[word]['polar'] == 'negative':
+	strongNeg += 1
+      elif sentimentWords[word]['polar'] == 'positve':
+        strongPos += 1
 
+      # checks if the majority of words are pos or neg
+      if strongPos == strongNeg:
+	taggedData.append('neutral')
+      elif strongPos > strongNeg:
+	taggedData.append('positive')
+      else:
+	taggedData.append('negative')
+  return taggedData
 
 
 def main():
   trainData = extractData( TRAINFILE )
   print trainData[0]
+  sentimentWords = getSentimentWords('sentimentLexicon.txt')
+  taggedData = useSentimentWordsOnly(sentimentWords, trainData)
+  # checkData(taggedData,trainData)
+  print taggedData
 
 
 if __name__=='__main__':
