d9bbd89b3a9bbbe239778c5bfed39b06dcac2a4c
Justin Cosentino <jcosent1@swarthmore.edu>
2012-11-28 01:52:19 -0500

    edits

--- sentimentTagger.py
+++ sentimentTagger.py
@@ -8,6 +8,7 @@ tweet.
 """
 from collections import defaultdict
 from sentimentWords import getSentimentWords
+from decision import *
 from random import *
 
 #some global values
@@ -65,15 +66,53 @@ def checkData(taggedData,trainData):
       count += 1
   print 'Percent Correct: %f (%d/%d)' %(float(correct)/count,correct,count)
      
+def useRules(trainDataB,rules):
+  taggedData = []
+  for ID in trainDataB.keys():
+    currentTweet = trainDataB[ID]['tweet']
+    pos = neg = neut = 0
+    for word in currentTweet.split():
+      print type(rules[word]['tag'])
+      if rules[word]['tag'] == "positive":
+        print word
+        pos += 1
+      elif rules[word]['tag'] == "negative":
+        neg += 1
+      else:
+        neut += 1
+ 
+    if pos > neg and pos > neut:
+      taggedData.append("positive")
+    elif neg > pos and neg > neut: 
+      taggedData.append('negative')
+    else:
+      taggedData.append('neutral')
+
+  return taggedData
+
+def checkDataB(taggedData,trainDataB):
+  correct = count = 0
+  for key in trainDataB.keys():
+    correctTag = trainDataB[key]['tag']
+    if correctTag == taggedData[count]:
+      correct += 1
+    count += 1 
+  print 'Percent Correct: %f (%d/%d)' %(float(correct)/count,correct,count)
 
 def main():
   trainData = extractData( TRAINFILE )
   sentimentWords = getSentimentWords('sentimentLexicon.txt')
   taggedData = useSentimentWordsOnly(sentimentWords, trainData)
   checkData(taggedData,trainData)
-  #print taggedData
-
+  
+  # using decision
+  trainingFileA = 'outputTestA.txt'
+  trainingFileB = 'outputTestB.txt'
+  trainDataA = parseA(trainingFileA)
+  trainDataB = parseB(trainingFileB)
+  rules = train(trainDataA,trainDataB)
+  taggedData = useRules(trainDataB,rules)
+  checkDataB(taggedData,trainDataB)
 
 if __name__=='__main__':
   main()
-
