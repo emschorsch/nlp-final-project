48727967964a44fc6d2560e51f04985c04493d9b
Justin Cosentino <jcosent1@swarthmore.edu>
2012-11-27 23:50:28 -0500

    attempted to use only sentiment lexicon, but this failed

--- sentimentTagger.py
+++ sentimentTagger.py
@@ -8,6 +8,7 @@ tweet.
 """
 from collections import defaultdict
 from sentimentWords import getSentimentWords
+from random import *
 
 #some global values
 TRAINFILE = 'outputTestA.txt'
@@ -18,7 +19,7 @@ TWEET = 5 #the index of the tweet itself
 builds a list of the data based on the info in filename
 """
 def extractData( fileName ):
-  #build the list of test words
+  #builda the list of test words
   f = open(TRAINFILE, 'r')
   trainData = map(lambda f: f.strip('\n').split('\t'), f.readlines())
   f.close()
@@ -28,33 +29,49 @@ def extractData( fileName ):
 def useSentimentWordsOnly(sentimentWords,trainData):
   taggedData = []
   for instance in trainData:
-    tweet = instance[TWEET]
-    strongNeg = weakNeg = strongPos = weakPos = 0
-    for word.strip('!@#$%^&*(),.?\"') in tweet:
+    if len(instance) == 6:
+      tweet = instance[TWEET]
+      neg = pos = 0
       
-      # counts the number of pos and neg words in the tweet
-      if sentimentWords[word]['polar'] == 'negative':
-	strongNeg += 1
-      elif sentimentWords[word]['polar'] == 'positve':
-        strongPos += 1
+      for word in tweet.split():
+        word = word.strip('!@#$%^&*(),.?\"') 
+   
+        # counts the number of pos and neg words in the tweet
+        if sentimentWords[word]['polar'] == 'negative':
+          neg += 1
+        elif sentimentWords[word]['polar'] == 'positve':
+          pos += 1
 
       # checks if the majority of words are pos or neg
-      if strongPos == strongNeg:
-	taggedData.append('neutral')
-      elif strongPos > strongNeg:
-	taggedData.append('positive')
+      if pos == neg:
+	if random() > .5:
+          taggedData.append('positive')
+        else:
+          taggedData.append('negative')
+      elif pos > neg:
+        taggedData.append('positive')
       else:
-	taggedData.append('negative')
+       taggedData.append('negative')
+
   return taggedData
 
+def checkData(taggedData,trainData):
+  count = correct =  0
+  for instance in trainData:
+    if len(instance) == 6:
+      tag = taggedData[count]
+      if tag == instance[POLAR]:
+        correct += 1
+      count += 1
+  print 'Percent Correct: %f (%d/%d)' %(float(correct)/count,correct,count)
+     
 
 def main():
   trainData = extractData( TRAINFILE )
-  print trainData[0]
   sentimentWords = getSentimentWords('sentimentLexicon.txt')
   taggedData = useSentimentWordsOnly(sentimentWords, trainData)
-  # checkData(taggedData,trainData)
-  print taggedData
+  checkData(taggedData,trainData)
+  #print taggedData
 
 
 if __name__=='__main__':
