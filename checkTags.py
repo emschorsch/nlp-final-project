e8294bc9b94fd50fa4a60bdba5fa6ebf9a7a13ef
Justin <jcosent1@swarthmore.edu>
2012-12-04 21:07:22 -0500

    program that checks the correctness of tags when given a dictionary containing the tags and the test data

---
+++ checkTags.py
@@ -0,0 +1,84 @@
+# ----------------------------------------------------------------- #
+
+def checkTagsA(tagsA,testA):
+  '''
+  Takes in a dictionay of the test data and guessed tags for Task A.
+  then determines the correctness of the tags, counting true and
+  false guesses for each sentiment class.
+  '''
+
+  TPos = TNeg = TObj = TNeut = FPos = FNeg = FObj = FNeut = 0.0
+  correct = total = 0.0
+
+  for ID in testA.keys():
+    for index in testA[ID].keys():
+
+      # Check if guess is corerct
+      polar = testA[ID][index]['polar']
+      guess = tagsA[ID][index]
+      if guess == polar:
+        correct += 1
+
+      # Keep track of true and false assignments  
+      if   guess == 'positive'  and guess == polar: TPos  += 1
+      elif guess == 'negative'  and guess == polar: TNeg  += 1
+      elif guess == 'neutral'   and guess == polar: TNeut += 1
+      elif guess == 'objective' and guess == polar: TObj  += 1
+
+      elif guess == 'positive'  and guess != polar: FPos  += 1
+      elif guess == 'negative'  and guess != polar: FNeg  += 1
+      elif guess == 'neutral'   and guess != polar: FNeut += 1
+      elif guess == 'objective' and guess != polar: FObj  += 1  
+
+      total += 1
+
+  print '[Task A] Percent Correct: %.2f (%.0f/%.0f)' \
+      %(correct/total, correct, total)
+  print "True Pos: %d\tFalse Pos: %d" %(TPos,FPos)
+  print "True Neg: %d\tFalse Neg: %d" %(TNeg,FNeg)
+  print "True Neut: %d\tFalse Neut: %d" %(TNeut,FNeut)
+  print "True Obj: %d\tFalse Obj: %d" %(TObj,FObj)
+
+# ----------------------------------------------------------------- #
+
+def checkTagsB(tagsB,testB):
+
+  '''
+  Takes in a dictionay of the test data and guessed tags for Task B.
+  then determines the correctness of the tags, counting true and
+  false guesses for each sentiment class.
+  '''
+
+  TPos = TNeg = TObj = TNeut = FPos = FNeg = FObj = FNeut = 0.0
+  correct = total = 0.0
+
+  for ID in testB.keys():
+    for subject in testB[ID].keys():
+
+      # Check if guess is corerct
+      polar = testB[ID][subject]['polar']
+      guess = tagsB[ID][subject]
+      if guess == polar:
+        correct += 1
+
+      # Keep track of true and false assignments  
+      if   guess == 'positive'  and guess == polar: TPos  += 1
+      elif guess == 'negative'  and guess == polar: TNeg  += 1
+      elif guess == 'neutral'   and guess == polar: TNeut += 1
+      elif guess == 'objective' and guess == polar: TObj  += 1
+
+      elif guess == 'positive'  and guess != polar: FPos  += 1
+      elif guess == 'negative'  and guess != polar: FNeg  += 1
+      elif guess == 'neutral'   and guess != polar: FNeut += 1
+      elif guess == 'objective' and guess != polar: FObj  += 1  
+
+      total += 1
+
+  print '[Task B] Percent Correct: %.2f (%.0f/%.0f)' \
+      %(correct/total, correct, total)
+  print "True Pos: %d\tFalse Pos: %d" %(TPos,FPos)
+  print "True Neg: %d\tFalse Neg: %d" %(TNeg,FNeg)
+  print "True Neut: %d\tFalse Neut: %d" %(TNeut,FNeut)
+  print "True Obj: %d\tFalse Obj: %d" %(TObj,FObj)
+
+# ----------------------------------------------------------------- #
