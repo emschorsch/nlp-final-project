410363f63590c5faf001d7045d2149c5fd383a6c
Justin <jcosent1@swarthmore.edu>
2012-12-07 15:43:48 -0500

    added a true random tagger, mfs sense tagger

--- randomTagger.py
+++ randomTagger.py
@@ -1,10 +1,10 @@
-from decisionParse import parseA, parseB
+from dataParse import parseA, parseB
 from sentimentWords import getSentimentWords
 from collections import defaultdict
 from random import *
 
 # ----------------------------------------------------------------- #
-def randomTagA(trainA, testA):
+def weightedRandomTag(train, test):
   '''
   Takes in two sources of data (test and train for a) and then 
   genterates tags based on the frequency of the tags found in 
@@ -12,91 +12,96 @@ def randomTagA(trainA, testA):
   '''
 
   # Get counts for trainA
-  posAC = negAC = neutAC = objAC = totalAC = 0.0
-  for ID in trainA.keys():
-    for index in trainA[ID].keys():
-      if trainA[ID][index]['polar'] == 'positive':  posAC  += 1
-      if trainA[ID][index]['polar'] == 'negative':  negAC  += 1
-      if trainA[ID][index]['polar'] == 'neutral':   neutAC += 1
-      if trainA[ID][index]['polar'] == 'objective': objAC  += 1
-      totalAC += 1
+  posC = negC = neutC = objC = totalC = 0.0
+  for ID in train.keys():
+    for index in train[ID].keys():
+      if train[ID][index]['polar'] == 'positive':  posC  += 1
+      if train[ID][index]['polar'] == 'negative':  negC  += 1
+      if train[ID][index]['polar'] == 'neutral':   neutC += 1
+      if train[ID][index]['polar'] == 'objective': objC  += 1
+      totalC += 1
   
-  print '[Task A] Pos: %.0f Neg: %.0f Neut: %.0f Obj: %.0f' \
-      %(posAC, negAC, neutAC, objAC)
+  print '\tCounts: Pos: %.0f Neg: %.0f Neut: %.0f Obj: %.0f' \
+      %(posC, negC, neutC, objC)
 
-  # Get percents for trainA
-  posAP  = posAC/totalAC
-  negAP  = negAC/totalAC
-  neutAP = neutAC/totalAC
-  objAP  = objAC/totalAC
+  # Get percents for train
+  posP  = posC/totalC
+  negP  = negC/totalC
+  neutP = neutC/totalC
+  objP  = objC/totalC
   
   # Use percents to randomly tag words and check correctness of tag
-  correctA = 0.0
-  for ID in testA.keys():
-    for index in testA[ID].keys():
-      currentPolar = testA[ID][index]['polar']
+  correct = 0.0
+  total = 0.0
+  for ID in test.keys():
+    for index in test[ID].keys():
+      currentPolar = test[ID][index]['polar']
       percent = random()
-      if percent > (1 - objAP): guess = 'objective'
-      elif percent > (1 - posAP - negAP): guess = 'negative'
-      elif percent > (1 - posAP - negAP - neutAP): guess = 'neutral'
+      if percent > (1 - objP): guess = 'objective'
+      elif percent > (1 - posP - negP): guess = 'negative'
+      elif percent > (1 - posP - negP - neutP): guess = 'neutral'
       else: guess = 'positive'
       if currentPolar == guess:
-        correctA += 1
+        correct += 1
+      total += 1
 
-  print '[Task A] Percent Correct:  %f (%.0f/%.0f)' \
-      %(correctA/totalAC, correctA, totalAC)
+  print '\tWeighted Random: Percent Correct:  %f (%.0f/%.0f)' \
+      %(correct/total, correct, total)
 # ----------------------------------------------------------------- #
-def randomTagB(trainB, testB):
-  '''
-  Takes in two sources of data (test and train for B) and then 
-  genterates tags based on the frequency of the tags found in 
-  the test data and applies them to the train data
-  '''
 
-  # Get counts for trainB
-  posBC = negBC = neutBC = objBC = totalBC = 0.0
-  for ID in trainB.keys():
-    for subject in trainB[ID].keys():
-      if trainB[ID][subject]['polar'] == 'positive':  posBC  += 1
-      if trainB[ID][subject]['polar'] == 'negative':  negBC  += 1
-      if trainB[ID][subject]['polar'] == 'neutral':   neutBC += 1
-      if trainB[ID][subject]['polar'] == 'objective': objBC  += 1
-      totalBC += 1
+def mfsTag(train,test):
+  pos = neg = obj = neut = 0
+  correct = total = 0.0
+  for ID in train.keys():
+    for index in train[ID].keys():
+      if train[ID][index]['polar'] == 'positive':  pos  += 1
+      if train[ID][index]['polar'] == 'negative':  neg  += 1
+      if train[ID][index]['polar'] == 'neutral':   neut += 1
+      if train[ID][index]['polar'] == 'objective': obj  += 1
   
-  print '[Task B] Pos: %.0f Neg: %.0f Neut: %.0f Obj: %.0f' \
-      %(posBC, negBC, neutBC, objBC)
-
-  # Get percents for trainB
-  posBP  = posBC/totalBC
-  negBP  = negBC/totalBC
-  neutBP = neutBC/totalBC
-  objBP  = objBC/totalBC
+  if pos > neg and pos > neut and pos > obj: mfs = 'positive'
+  elif neg > pos and neg > neut and neg > obj: mfs = 'negative'
+  elif neut > neg and neut > pos and neut > obj: mfs = 'neutral'
+  elif obj > neg and obj > neut and obj > pos: mfs = 'objective'
   
-  print negBP, posBP
+  for ID in test.keys():
+    for index in test[ID].keys():
+      if test[ID][index]['polar'] == mfs:
+        correct += 1
+      total += 1
 
-  # Use percents to randomly tag words and check correctness of tag
-  correctB = 0.0
-  for ID in testB.keys():
-    for subject in testB[ID].keys():
-      currentPolar = testB[ID][subject]['polar']
-      percent = random()
-      if percent > (1 - objBP): guess = 'objective'
-      elif percent > (1 - posBP - negBP): guess = 'negative'
-      elif percent > (1 - posBP - negBP - neutBP): guess = 'neutral'
-      else: guess = 'positive'
-
-      if currentPolar == guess:
-        correctB += 1
-
-  print '[Task B] Percent Correct:  %f (%.0f/%.0f)' \
-      %(correctB/totalBC, correctB, totalBC)
+  print '\tMost Frequent (%s): Percent Correct:  %f (%.0f/%.0f)' \
+      %(mfs, correct/total, correct, total)
 
 # ----------------------------------------------------------------- #
+
+def trueRandom(train, test):
+  correct = total = 0.0
+  polars = ['positive','negative','neutral','objective']
+  for ID in test.keys():
+    for index in test[ID].keys():
+      randomChoice = choice(polars)
+      if randomChoice == test[ID][index]['polar']: 
+        correct += 1
+      total += 1
+
+  print '\tRandom: Percent Correct:  %f (%.0f/%.0f)' \
+      %(correct/total, correct, total)
+
+# ----------------------------------------------------------------- #
+
 if __name__ == '__main__':
   trainingFileA = 'trainA.txt'
   trainingFileB = 'trainB.txt'
-  trainA  = parseA(trainingFileA)
-  trainB  = parseB(trainingFileB)
-  randomTagA(trainA, trainA)
-  randomTagB(trainB, trainB)
+  trainA, testA = parseA(trainingFileA)
+  trainB, testB = parseB(trainingFileB)
+  print "[TASK A]"
+  weightedRandomTag(trainA, testA)
+  mfsTag(trainA, testA)
+  trueRandom(trainA, testA)
+  print "[TASK B]"
+  weightedRandomTag(trainB, testB)
+  mfsTag(trainB, testB)
+  trueRandom(trainB, testB)
+
 # ----------------------------------------------------------------- #
