17e6aef7f976bd28918e01130ab0cab0e378b723
Justin <jcosent1@swarthmore.edu>
2012-12-16 15:23:58 -0500

    wooooohoooooooo it works

--- decisionList.py
+++ decisionList.py
@@ -4,6 +4,7 @@ from dataParse import parseA, parseB
 from collections import defaultdict
 from operator import itemgetter
 from checkTags import checkTagsB
+TWEET = 5
 # ----------------------------------------------------------------- #
 
 def getScores(scores,mfp):
@@ -25,6 +26,7 @@ def getScores(scores,mfp):
 
 def getTags(rules,test):
   
+  '''
   total = correct = 0.0
   for ID in test.keys():
     for index in test[ID].keys():
@@ -33,31 +35,58 @@ def getTags(rules,test):
       for ruleSet in rules:
         rule,polar,score = ruleSet[0],ruleSet[1],ruleSet[2]
         if rule in currentTweet and score > 0:
-          if polar ==  currentPolar: correct += 1
-          break
+          if polar ==  currentPolar: 
+            correct += 1
+            break
         else:
-          if 'objective' == currentPolar: correct += 1
-          break
+          if 'positive' == currentPolar: 
+            correct += 1
+            break
       total += 1
   print '\tDecision List: Percent Correct:  %f (%.0f/%.0f)' \
       %(correct/total, correct, total)
+  '''
+  tags = defaultdict(lambda: defaultdict(str))
+  
+  for ID in test.keys():
+    for index in test[ID].keys():
+      currentTweet = test[ID][index]['tweet'].split()
+      editedTweet = []
+      for word in currentTweet:
+        editedTweet.append(word.lower().strip('\'\"!@#$%^&*(),.?<>;:-'))
+      ' '.join(editedTweet)
+      taggedFlag = False
+      for ruleSet in rules:
+        rule, polar, score = ruleSet[0], ruleSet[1], ruleSet[2]
+        if rule in editedTweet and score > 0:
+          tags[ID][index] = polar
+          taggedFlag = True
+          break
+      if not taggedFlag:
+        tags[ID][index] = 'positive'
+  return tags
 
 # ----------------------------------------------------------------- #
 
-def decisionList(train,test):
-  decList = []
-  mfp = defaultdict(lambda: defaultdict(int))
-  scores = defaultdict(lambda: defaultdict(int))
-
-  # get counts of the mfp (most frequent polarity) of each unigram and bigram
+def getUnigrams(train):
+  mfp = defaultdict(lambda: defaultdict(int))  
   for ID in train.keys():
     for subj in train[ID].keys():
       tweet = train[ID][subj]['tweet'].split()
       for word in tweet:
-	word1 = word.lower().strip('\'\"!@#$%^&*(),.?<>;:-')
+        word1 = word.lower().strip('\'\"!@#$%^&*(),.?<>;:-')
         polar = train[ID][subj]['polar']
         mfp[word1][polar] += 1
         mfp[word1]['count'] += 1
+  return mfp
+
+# ----------------------------------------------------------------- #
+
+def getBigrams(train):
+  mfp = defaultdict(lambda: defaultdict(int))  
+  for ID in train.keys():
+    for subj in train[ID].keys():
+      tweet = train[ID][subj]['tweet'].split()
       for i in range(1,len(tweet)):
         w1 = tweet[i-1].lower().strip('\'\"!@#$%^&*(),.?<>;:')
 	w2 = tweet[i].lower().strip('\'\"!@#$%^&*(),.?<>;:')
@@ -65,6 +94,39 @@ def decisionList(train,test):
         polar = train[ID][subj]['polar']
         mfp[bigram][polar] += 1
         mfp[bigram]['count'] += 1
+  return mfp
+
+# ----------------------------------------------------------------- #
+
+def unigramsBigrams(train):
+  mfp = defaultdict(lambda: defaultdict(int))  
+  for ID in train.keys():
+    for subj in train[ID].keys():
+      tweet = train[ID][subj]['tweet'].split()
+      for i in range(1,len(tweet)):
+        w1 = tweet[i-1].lower().strip('\'\"!@#$%^&*(),.?<>;:')
+	w2 = tweet[i].lower().strip('\'\"!@#$%^&*(),.?<>;:')
+        bigram = w1 + ' ' + w2
+        polar = train[ID][subj]['polar']
+        mfp[bigram][polar] += 1
+        mfp[bigram]['count'] += 1
+      for word in tweet:
+        word1 = word.lower().strip('\'\"!@#$%^&*(),.?<>;:-')
+        polar = train[ID][subj]['polar']
+        mfp[word1][polar] += 1
+        mfp[word1]['count'] += 1
+  return mfp
+
+
+# ----------------------------------------------------------------- #
+
+def decisionList(train,test):
+  scores = defaultdict(lambda: defaultdict(int))
+
+  # get counts of the mfp (most frequent polarity)
+  mfp = getUnigrams(train)
+  #mfp = getBigrams(train)
+  #mfp = unigramsBigrams(train)
   
   # get the highest score for each unigram and its associated polarity
   getScores(scores,mfp)
@@ -82,8 +144,9 @@ def decisionList(train,test):
 # ----------------------------------------------------------------- #      
 
 if __name__ == '__main__':
-  trainingFile = 'trainB.txt'
-  trainB, testB = parseB(trainingFile)
-  tags = decisionList(trainB,testB)
+  trainingFile = 'trainA.txt'
+  train, test = parseA(trainingFile)
+  tags = decisionList(train,test)
+  checkTagsB(tags,test)
 
 # ----------------------------------------------------------------- #
